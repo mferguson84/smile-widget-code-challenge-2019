b06777946d9088046fd3241a14adceeb5c64e8ab
mferguson84 <meferguson@icloud.com>
2019-07-09 21:34:00 -0500

    Added product price feature

--- smilewidgets/products/models.py
+++ smilewidgets/products/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=25, help_text='Customer facing name of product')
-    code = models.CharField(max_length=10, help_text='Internal facing reference to product')
-    price = models.PositiveIntegerField(help_text='Price of product in cents')
+    code = models.CharField(max_length=10, unique=True, help_text='Internal facing reference to product')
     
     def __str__(self):
         return '{} - {}'.format(self.name, self.code)
@@ -22,3 +21,17 @@ class GiftCard(models.Model):
     @property
     def formatted_amount(self):
         return '${0:.2f}'.format(self.amount / 100)
+
+
+class ProductPrice(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField(help_text='Price of product in cents')
+    date_start = models.DateField()
+    date_end = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return '{} - {} ({:%m/%d/%Y}-{})'.format(self.product.name, self.formatted_price, self.date_start, '{:%m/%d/%Y}'.format(self.date_end) if self.date_end else '')
+
+    @property
+    def formatted_price(self):
+        return '${0:.2f}'.format(self.price / 100)
--- smilewidgets/products/views.py
+++ smilewidgets/products/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.utils.dateparse import parse_date
+from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from products.models import Product, ProductPrice, GiftCard
 
 # Create your views here.
+@csrf_exempt
+def get_price(request):
+    """
+    Return a price for a given product, date, and (optional) gift card code.
+    """
+    if request.method != 'GET':
+        return JsonResponse({
+            'message': 'invalid method; only GET is supported',
+            'success': False
+        })
+
+    product_code = request.GET.get('productCode', '')
+    date = request.GET.get('date', '')
+    gift_card_code = request.GET.get('giftCardCode')
+
+    if not product_code:
+        return JsonResponse({
+            'message': 'productCode is mandatory',
+            'success': False
+        })
+
+    if not date:
+        return JsonResponse({
+            'message': 'date is mandatory',
+            'success': False
+        })
+
+    parsed_date = parse_date(date)
+    if not parsed_date:
+        print(date)
+        return JsonResponse({
+            'message': 'invalid date format (YYYY-MM-DD required)',
+            'success': False
+        })
+
+    try:
+        product = Product.objects.get(code=product_code)
+    except ObjectDoesNotExist:
+        return JsonResponse({
+            'message': 'could not find product with code {}'.format(product_code),
+            'success': False
+        })
+    
+    if gift_card_code:
+        gift_card_query = GiftCard.objects.filter(code=gift_card_code, date_start__lte=parsed_date).order_by('-amount')
+        for gift_card in gift_card_query.all():
+            if not gift_card.date_end or gift_card.date_end >= parsed_date:
+                break
+        else:
+            gift_card = None
+    else:
+        gift_card = None
+
+    product_prices = product.productprice_set.filter(date_start__lte=parsed_date).order_by('price')
+    for product_price in product_prices.all():
+        if not product_price.date_end or product_price.date_end >= parsed_date:
+            if not gift_card:
+                price = product_price.formatted_price
+            elif gift_card.amount < product_price.price:
+                price = '${0:.2f}'.format((product_price.price - gift_card.amount) / 100)
+            else:
+                price = '$0.00'
+
+            response = {
+                'price': price,
+                'success': True
+            }
+            if gift_card_code and not gift_card:
+                response['message'] = 'gift card code {} not valid'.format(gift_card_code)
+            elif gift_card_code and gift_card:
+                response['message'] = 'gift card code {} applied'.format(gift_card_code)
+
+            return JsonResponse(response)
+    else:
+        return JsonResponse({
+            'message': 'could not find a price for that product',
+            'success': False
+        })
